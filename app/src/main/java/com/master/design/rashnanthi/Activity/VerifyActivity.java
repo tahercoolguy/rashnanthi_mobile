e776e8f5079143182ddc9c10fd2439f3636fc0a2
Chirag <chirag22soni04@gmail.com>
2022-01-15 17:26:10 +0530

    First Commit By Chirag

---
+++ app/src/main/java/com/master/design/rashnanthi/Activity/VerifyActivity.java
@@ -0,0 +1,15 @@
+package com.master.design.rashnanthi.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.master.design.rashnanthi.R;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
---
+++ app/src/main/java/com/master/design/rashnanthi/Utils/RecyclerViewHolders.java
@@ -0,0 +1,24 @@
+package com.master.design.rashnanthi.Utils;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.master.design.rashnanthi.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolders extends RecyclerView.ViewHolder{
+
+    private static final String TAG = RecyclerViewHolders.class.getSimpleName();
+
+    public TextView profileHeader;
+
+    public TextView profileContent;
+
+    public RecyclerViewHolders(final View itemView) {
+        super(itemView);
+        profileHeader = (TextView)itemView.findViewById(R.id.heading);
+        profileContent = (TextView) itemView.findViewById(R.id.profile_content);
+    }
+}
