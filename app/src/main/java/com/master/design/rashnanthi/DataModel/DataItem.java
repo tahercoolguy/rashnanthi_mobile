e7132a2ac84bd123cc6ce044dc9eb365ab9c812a
Chirag Soni <chirag22soni04@gmail.com>
2022-08-22 13:26:32 +0530

    22-08-2022 social media page

--- app/src/main/java/com/master/design/rashnanthi/DataModel/DataItem.java
+++ app/src/main/java/com/master/design/rashnanthi/DataModel/DataItem.java
@@ -1,86 +1,67 @@
 package com.master.design.rashnanthi.DataModel;
 
 public class DataItem{
-	private String youtube;
+	private String id;
+	private String facebook;
 	private String snapchat;
 	private String twitter;
- 	private String tiktok;
-	private String facebook;
+	private String youtube;
+	private String tiktok;
 	private String telegram;
-	private String id;
 	private String instagram;
-
-	public String getWhatsapp() {
-		return whatsapp;
+	private String whatsap;
+	public String getId() {
+		return id;
 	}
-
-	public void setWhatsapp(String whatsapp) {
-		this.whatsapp = whatsapp;
+	public void setId(String id) {
+		this.id = id;
 	}
-
-	private String whatsapp;
-
+	public String getFacebook() {
+		return facebook;
+	}
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+	public String getSnapchat() {
+		return snapchat;
+	}
+	public void setSnapchat(String snapchat) {
+		this.snapchat = snapchat;
+	}
+	public String getTwitter() {
+		return twitter;
+	}
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+	public String getYoutube() {
+		return youtube;
+	}
+	public void setYoutube(String youtube) {
+		this.youtube = youtube;
+	}
+	public String getTiktok() {
+		return tiktok;
+	}
+	public void setTiktok(String tiktok) {
+		this.tiktok = tiktok;
+	}
+	public String getTelegram() {
+		return telegram;
+	}
+	public void setTelegram(String telegram) {
+		this.telegram = telegram;
+	}
 	public String getInstagram() {
 		return instagram;
 	}
-
 	public void setInstagram(String instagram) {
 		this.instagram = instagram;
 	}
-
-	public void setYoutube(String youtube){
-		this.youtube = youtube;
+	public String getWhatsap() {
+		return whatsap;
 	}
-
-	public String getYoutube(){
-		return youtube;
-	}
-
-	public void setSnapchat(String snapchat){
-		this.snapchat = snapchat;
-	}
-
-	public String getSnapchat(){
-		return snapchat;
-	}
-
-	public void setTwitter(String twitter){
-		this.twitter = twitter;
-	}
-
-	public String getTwitter(){
-		return twitter;
-	}
-
-	public void setTiktok(String tiktok){
-		this.tiktok = tiktok;
-	}
-
-	public String getTiktok(){
-		return tiktok;
-	}
-
-	public void setFacebook(String facebook){
-		this.facebook = facebook;
-	}
-
-	public String getFacebook(){
-		return facebook;
-	}
-
-	public void setTelegram(String telegram){
-		this.telegram = telegram;
-	}
-
-	public String getTelegram(){
-		return telegram;
-	}
-
-	public void setId(String id){
-		this.id = id;
-	}
-
-	public String getId(){
-		return id;
+	public void setWhatsap(String whatsap) {
+		this.whatsap = whatsap;
 	}
 }
--- app/src/main/java/com/master/design/rashnanthi/Fragments/Social_Media_Fragment.java
+++ app/src/main/java/com/master/design/rashnanthi/Fragments/Social_Media_Fragment.java
@@ -31,6 +31,8 @@ import com.master.design.rashnanthi.R;
 import com.master.design.rashnanthi.Utils.ConnectionDetector;
 import com.master.design.rashnanthi.Utils.Helper;
 
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.sephiroth.android.library.widget.HListView;
@@ -89,70 +91,95 @@ public class Social_Media_Fragment extends Fragment {
                     progress.dismiss();
                     if (socialMediaDM.getOutput().getSuccess().equalsIgnoreCase("1")) {
 
-
-                        if (socialMediaDM.getOutput().getData().get(0).getInstagram() != null) {
-//
-
-                        } else {
+                        String insta = socialMediaDM.getOutput().getData().get(0).getInstagram();
+                        if (insta.isEmpty()) {
                             instaRL.setVisibility(View.GONE);
-                        }
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getWhatsapp() != null) {
-//
-
                         } else {
+                            instaRL.setVisibility(View.VISIBLE);
+                        }
+//                        if (insta!=null) {
+//                            instaRL.setVisibility(View.VISIBLE);
+//
+//                        } else {
+//                            instaRL.setVisibility(View.GONE);
+//                        }
+
+                        String whatsapp = socialMediaDM.getOutput().getData().get(0).getWhatsap();
+                        if (whatsapp.isEmpty()) {
                             whatsappRl.setVisibility(View.GONE);
-                        }
-//
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getTwitter() != null) {
-//
 
                         } else {
+                            whatsappRl.setVisibility(View.VISIBLE);
+                        }
+//                         else {
+//                            whatsappRl.setVisibility(View.GONE);
+//                        }
+////
+
+                        String twitter = socialMediaDM.getOutput().getData().get(0).getTwitter();
+                        if (twitter.isEmpty()) {
+//
                             twitterRL.setVisibility(View.GONE);
-                        }
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getSnapchat() != null) {
-//
-
                         } else {
+                            twitterRL.setVisibility(View.VISIBLE);
+                        }
+//                        else {
+//                            twitterRL.setVisibility(View.GONE);
+//                        }
+
+                        String snapchat = socialMediaDM.getOutput().getData().get(0).getSnapchat();
+                        if (snapchat.isEmpty()) {
                             snapchatRl.setVisibility(View.GONE);
-                        }
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getYoutube() != null) {
-//
 
                         } else {
+                            snapchatRl.setVisibility(View.VISIBLE);
+                        }
+//                        else {
+//                            snapchatRl.setVisibility(View.GONE);
+//                        }
+
+                        String youtube = socialMediaDM.getOutput().getData().get(0).getYoutube();
+                        if (youtube.isEmpty()) {
+//
                             youtubeRL.setVisibility(View.GONE);
-                        }
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getTiktok() != null) {
-//
-
                         } else {
+                            youtubeRL.setVisibility(View.VISIBLE);
+                        }
+//                        else {
+//                            youtubeRL.setVisibility(View.GONE);
+//                        }
+
+                        String tiktok = socialMediaDM.getOutput().getData().get(0).getTiktok();
+                        if (tiktok.isEmpty()) {
                             tiktokRl.setVisibility(View.GONE);
-                        }
-
-
-                        if (socialMediaDM.getOutput().getData().get(0).getTelegram() != null) {
-//
-
                         } else {
+                            tiktokRl.setVisibility(View.VISIBLE);
+                        }
+//                        else {
+//                            tiktokRl.setVisibility(View.GONE);
+//                        }
+
+                        String telegram = socialMediaDM.getOutput().getData().get(0).getTelegram();
+                        if (telegram.isEmpty()) {
+//
                             telegramRL.setVisibility(View.GONE);
+                        } else {
+                            telegramRL.setVisibility(View.VISIBLE);
                         }
+//                        else {
+//                            telegramRL.setVisibility(View.GONE);
+//                        }
 
-
-                        if (socialMediaDM.getOutput().getData().get(0).getFacebook() != null) {
-//
+                        String facebook = socialMediaDM.getOutput().getData().get(0).getFacebook();
+                        if (facebook.isEmpty()) {
+                            facbookRL.setVisibility(View.GONE);
 
                         } else {
-                            facbookRL.setVisibility(View.GONE);
+                            facbookRL.setVisibility(View.VISIBLE);
                         }
+//                        else {
+//                            facbookRL.setVisibility(View.GONE);
+//                        }
 
 
                     } else
@@ -167,9 +194,7 @@ public class Social_Media_Fragment extends Fragment {
                 }
             });
         } else
-            Helper.showToast(context,
-
-                    getString(R.string.no_internet_connection));
+            Helper.showToast(context, getString(R.string.no_internet_connection));
     }
 
 
@@ -192,10 +217,30 @@ public class Social_Media_Fragment extends Fragment {
 //                                startActivity(intent);
 //                                Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getInstagram()));
 
-                                String url = socialMediaDM.getOutput().getData().get(0).getInstagram();
+                                String instagram = socialMediaDM.getOutput().getData().get(0).getInstagram();
+//                                String url = instagram;
+//                                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                                intent.setData(Uri.parse(url));
+//                                startActivity(intent);
+
+                                Uri appUri = Uri.parse("https://instagram.com/" + instagram);
+//                                Uri browserUri = Uri.parse("https://instagram.com/" + instagram);
+
+//                                try { //first try to open in instagram app
+//                                    Intent appIntent = getActivity().getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+//                                    if (appIntent != null) {
+//                                        appIntent.setAction(Intent.ACTION_VIEW);
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                                intent.setData(Uri.parse(url));
+//                                        intent.setData(appUri);
+//                                        startActivity(intent);
+                                intent.setData(appUri);
                                 startActivity(intent);
+//                                    }
+//                                } catch (Exception e) { //or else open in browser
+//                                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, appUri);
+//                                    startActivity(browserIntent);
+//                                }
+
 
                             } else {
                                 instaRL.setVisibility(View.GONE);
@@ -204,17 +249,28 @@ public class Social_Media_Fragment extends Fragment {
                         }
                         if (ifwhatsapp != false) {
 
-                            if (socialMediaDM.getOutput().getData().get(0).getWhatsapp() != null) {
+                            if (socialMediaDM.getOutput().getData().get(0).getWhatsap() != null) {
 //                                String url = socialMediaDM.getOutput().getData().get(0).getWhatsapp();
 //                                Intent intent =!=false new Intent(Intent.ACTION_VIEW);
 //                                intent.setData(Uri.parse(url));
 //                                startActivity(intent);
 //                                Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getInstagram()));
-
-                                String url = socialMediaDM.getOutput().getData().get(0).getWhatsapp();
+//
+                                String whatsapp = socialMediaDM.getOutput().getData().get(0).getWhatsap();
+                                String str = whatsapp;
+                                int n = 2;
+                                String newwhatsappnum = str.substring(n);
+                                System.out.println(str.substring(n));
+                                String url = "https://api.whatsapp.com/send?phone=" + newwhatsappnum;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
+
+
+//                                String url = socialMediaDM.getOutput().getData().get(0).getWhatsap();
+//                                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                                intent.setData(Uri.parse(url));
+//                                startActivity(intent);
 
                             } else {
                                 whatsappRl.setVisibility(View.GONE);
@@ -231,8 +287,8 @@ public class Social_Media_Fragment extends Fragment {
 //                                intent.setData(Uri.parse(url));
 //                                startActivity(intent);
 //                                Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getTwitter()));
-
-                                String url = socialMediaDM.getOutput().getData().get(0).getTwitter();
+                                String twitter=socialMediaDM.getOutput().getData().get(0).getTwitter();
+                                String url = "https://twitter.com/"+twitter;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
@@ -250,8 +306,9 @@ public class Social_Media_Fragment extends Fragment {
 //                                intent.setData(Uri.parse(url));
 //                                startActivity(intent);
 //                                Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getSnapchat()));
-
-                                String url = socialMediaDM.getOutput().getData().get(0).getSnapchat();
+                                String snapchat = socialMediaDM.getOutput().getData().get(0).getSnapchat();
+                                String snaplower = snapchat.toLowerCase(Locale.ROOT);
+                                String url = "https://www.snapchat.com/add/" + snaplower;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
@@ -270,7 +327,8 @@ public class Social_Media_Fragment extends Fragment {
 //                                startActivity(intent);
 //                              Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getYoutube()));
 
-                                String url = socialMediaDM.getOutput().getData().get(0).getYoutube();
+                                String youtube = socialMediaDM.getOutput().getData().get(0).getYoutube();
+                                String url = "https://youtube.com/" + youtube;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
@@ -288,8 +346,8 @@ public class Social_Media_Fragment extends Fragment {
 //                                intent.setData(Uri.parse(url));
 //                                startActivity(intent);
 //                               Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getTiktok()));
-
-                                String url = socialMediaDM.getOutput().getData().get(0).getTiktok();
+                                String tiktok=socialMediaDM.getOutput().getData().get(0).getTiktok();
+                                 String url = "https://www.tiktok.com/"+tiktok;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
@@ -309,7 +367,11 @@ public class Social_Media_Fragment extends Fragment {
 //                                startActivity(intent);
 //                               Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getTelegram()));
 
-                                String url = socialMediaDM.getOutput().getData().get(0).getTelegram();
+                                String telegram = socialMediaDM.getOutput().getData().get(0).getTelegram();
+                                String str = telegram;
+                                int n = 2;
+                                String newtelegram = str.substring(n);
+                                String url = "http://telegram.me/shes_ir" + newtelegram;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
@@ -328,7 +390,9 @@ public class Social_Media_Fragment extends Fragment {
 //                                startActivity(intent);
 //                               Helper.showToast(context, String.valueOf(socialMediaDM.getOutput().getData().get(0).getTelegram()));
 
-                                String url = socialMediaDM.getOutput().getData().get(0).getFacebook();
+
+                                String fb=socialMediaDM.getOutput().getData().get(0).getFacebook();
+                                String url = "http://www.facebook.com/"+fb;
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
                                 intent.setData(Uri.parse(url));
                                 startActivity(intent);
